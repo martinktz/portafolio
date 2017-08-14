2fab5bc63e32035bc27daa040523aa627a77b4a9
Martin Kasañetz <kasanetz.martin@gmail.com>
2017-08-14 15:55:13 -0300

    componente item creado

---
+++ src/app/components/item/item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html'
+})
+export class ItemComponent {
+
+  constructor() { }
+
+
+}
