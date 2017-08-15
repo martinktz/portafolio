2a2f6f7683edd93d86dac9212ca112523ea165b8
Martin Kasañetz <kasanetz.martin@gmail.com>
2017-08-15 17:30:20 -0300

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,16 +6,32 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean=false;
+  cargada_sobre_nosotros:boolean=false;
+  equipo:any=[];
+  url_equipo:string="https://paginaweb-9ca1f.firebaseio.com/equipo.json";
 
   constructor( public http:Http ) {
 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+
+  }
+
+  carga_info(){
     this.http.get("assets/data/info.pagina.json")
       .subscribe( data => {
-        console.log(data.json());
         this.info = data.json();
         this.cargada = true;
       })
+  }
 
+  carga_sobre_nosotros() {
+    this.http.get( this.url_equipo )
+      .subscribe( data => {
+        console.log(data.json())
+        this.equipo = data.json();
+        this.cargada_sobre_nosotros = true;
+      })
   }
 
 }
